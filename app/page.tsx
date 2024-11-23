5bdcb38df85b933fef7eeb96808c3de1d5321e3d
liora_ves <lioravesnovaty@gmail.com>
2024-11-23 15:58:07 +0200

    added explanations in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,20 @@ export default function Home() {
         <h3>This web application includes:</h3>
         <ul>
           <li>
-            Part 1 - <Link href="/tic-tac-toe">Tic Tac Toe Game</Link>
+            Part 1 - <Link href="/tic-tac-toe">Tic Tac Toe Game</Link>:
+            <p>A simple and interactive game where two players can compete.</p>
           </li>
           <li>
-            Part 2 - <Link href="/nasa">NASA API Data Fetching</Link>
+            Part 2 - <Link href="/nasa">NASA API Data Fetching</Link>:
+            <p>A page that fetches and displays data from NASA API.</p>
           </li>
           <li>
-            Part 3 - <Link href="/design">Design to Code</Link>
+            Part 3 - <Link href="/design">Design to Code</Link>:
+            <p>A design-to-code project with a login, success, and failure page, designed by Noga Aloni.<br/>
+              Includes animations to enhance the user experience.<br/>
+              While not connected to a database, the failure page is accessible via the navbar.
+            </p>
+
           </li>
         </ul>
         <p>Feel free to explore these sections using the links above or the navigation bar!</p>
